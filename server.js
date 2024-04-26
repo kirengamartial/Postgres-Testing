d9fb3024abb552994aaab1fb14d089d4ae2f9a73
kirengamartial <kirengamartial@gmail.com>
2024-04-26 21:09:13 +0200

    New changes

--- server.js
+++ server.js
@@ -7,4 +7,4 @@ app.use(express.json())
 
 app.use('/', QueriesRoutes)
 
-app.listen(3000, () => console.log('app is listening on port 3000'))
+app.listen(4000, () => console.log('app is listening on port 4000'))
