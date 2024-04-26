cee6db9d003ac5870dc41095cd1e2921a435eb7a
kirengamartial <kirengamartial@gmail.com>
2024-04-26 21:42:01 +0200

    new changes

--- api/index.js
+++ api/index.js
@@ -1,10 +1,11 @@
 const express = require('express')
-const QueriesRoutes = require('../routes/crudRoutes') 
 require('dotenv').config()
 
 const app = express()
 app.use(express.json())
 
-app.use('/', QueriesRoutes)
+app.use('/',(req, res) => {
+    res.send('Api is running really well')
+})
 
 app.listen(4000, () => console.log('app is listening on port 4000'))
